5f2a6fbf5b2fb00e11ed8a73b973da9944da3330
Seiya-wasabi <se.sato614@gmail.com>
2021-04-17 04:06:02 +0000

    topページとヘッダー完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  # resources :homes
+ 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   resources :homes
   resources :books
